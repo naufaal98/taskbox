9c4d1071cdc2c6086f0b3cb7ca44c2a641d0d067
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-02-28 23:29:25 +0700

    taskbox UI

---
+++ storybook-static/main.11fa0cd543d58f4f2f22.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){p(420),p(562),p(1295),p(1231),n.exports=p(1262)},function(n,o){}],[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.56bd20846221961ffd543904d6d9440c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b624490477c944c884ae2b90865dc7f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ede23d89edf2b788367f.bundle.js"
+  },
+  {
+    "url": "runtime~main.ede23d89edf2b788367f.bundle.js"
+  },
+  {
+    "revision": "2fad6a6be9ce3068562d",
+    "url": "static/css/main.811abe8a.chunk.css"
+  },
+  {
+    "url": "vendors~main.ede23d89edf2b788367f.bundle.js"
+  },
+  {
+    "url": "vendors~main.ede23d89edf2b788367f.bundle.js.LICENSE.txt"
+  }
+]);
